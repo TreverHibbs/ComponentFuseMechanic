7608cf3155ba2a2ae77af79aad76560f729ce54e
Trever Hibbs <treverhibbs@gmail.com>
2026-03-27 16:30:01 -0700

    add networked physics cube

--- Source/Fuse/Fuse.Build.cs
+++ Source/Fuse/Fuse.Build.cs
@@ -6,10 +6,7 @@
 //TODO look into animated the wheels so that only the wheel bone moves and not
 //the axel
 //TODO set up chaos mover character
-//  TODO set up input event binding system with caching
-//  TODO set up input for camera rotation
 //  TODO set up and test networking
-//  TODO look into why physics collision doesn't seem to function properly
 //TODO implement simple as possible dynamic modular vehicle building system
 //using chaos move character as base
 //TODO set up basic dynamic interaction activation system for modular vehicle
